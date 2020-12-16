3d918b395c332a8954ec4217bf7b3d2e44e46340
Richard-Drogo <richard.drogo@gmail.com>
2020-12-16 09:42:09 +0100

    Extra modules added, db.config.js added to configure connection to PostgreSQL's behavior (small things TODO yet), new model added.

--- ArchitectureNTiersApp/index.js
+++ ArchitectureNTiersApp/index.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
-const port = 8000;
+const db = require("./models");
+const origin_port = 8000;
+const listening_port = 8001;
+
+var corsOptions = {
+  origin: "http://localhost:8000".concat(origin_port.toString())
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json()); // Parse requests of content-type: application/json
+app.use(bodyParser.urlencoded({extended: true})); // Parse requests of content-type: application/x-www-form-urlencoded
+
+//db.sequelize.sync(); // Production Mode
+
+/*
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db."); // <- Hint about what this do for development Mode.
+});
+*/
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World!')
+  res.json({ message: "Hello World" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+app.listen(listening_port, () => {
+  console.log(`Example app listening on port ${listening_port}!`)
 });
